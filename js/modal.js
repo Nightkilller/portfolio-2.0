b266bc01bc0d68ff082009badf9e8b446d2627a1
Nightkilller <guptaa17aug@gmail.com>
2026-07-25 23:37:21 +0530

    add interactive Resume Modal with in-site preview and PDF download link

--- js/modal.js
+++ js/modal.js
@@ -7,6 +7,7 @@ class ModalController {
     this.projectModal = document.getElementById('project-modal');
     this.bookingModal = document.getElementById('booking-modal');
     this.connectModal = document.getElementById('connect-modal');
+    this.resumeModal = document.getElementById('resume-modal');
     this.activeModal = null;
   }
 
@@ -15,6 +16,17 @@ class ModalController {
     this.bindBackdropClicks();
     this.bindEscKey();
     this.bindBookingForm();
+    this.bindResumeLink();
+  }
+
+  bindResumeLink() {
+    const resumeLink = document.getElementById('sidebar-resume-link');
+    if (resumeLink) {
+      resumeLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.openModal(this.resumeModal);
+      });
+    }
   }
 
   bindCloseButtons() {
